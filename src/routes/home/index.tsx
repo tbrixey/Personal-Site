a74e989121235508272eccea2c591b1fab0d5ac9
Trevor B <t.brixey0129@gmail.com>
2020-06-24 01:28:23 -0500

    personal site updates! should be able to launch it sometime soon

--- src/routes/home/index.tsx
+++ src/routes/home/index.tsx
@@ -1,15 +1,26 @@
 import { FunctionalComponent, h } from "preact";
 import * as style from "./style.css";
+import { Link } from "preact-router";
 
 const Home: FunctionalComponent = () => {
     return (
         <div class={style.container}>
             <h1>My name is Trevor 🤙</h1>
-            <h2>Full-Stack Developer and aspiring designer</h2>
-            <p>
-                I currently work for a small agency while also picking up
+            <h2>Full-Stack Developer and aspiring designer ✨</h2>
+            <div class={style.section}>
+                I currently work at a small agency called{" "}
+                <a href={"https://crema.us"}>Crema</a> while also picking up
                 freelance jobs.
-            </p>
+            </div>
+            <div class={style.section}>
+                Check out my portfolio{" "}
+                <Link
+                    href="/portfolio"
+                    style={{ color: "#147efb", textDecoration: "none" }}
+                >
+                    here
+                </Link>
+            </div>
         </div>
     );
 };
--- src/routes/portfolio/index.tsx
+++ src/routes/portfolio/index.tsx
@@ -4,9 +4,36 @@ import * as style from "./style.css";
 const Portfolio: FunctionalComponent = () => {
     return (
         <div class={style.container}>
-            <h1>Portfolio</h1>
-            <h4>Personal</h4>
-            <h4>Freelance</h4>
+            <div class={style.section}>
+                <h2>Personal</h2>
+                <div>
+                    <h4>Baby Companion</h4>
+                    <ul>
+                        <li>
+                            <p>React Native</p>
+                        </li>
+                        <li>
+                            <p>Firebase</p>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+            <div class={style.section}>
+                <h2>Professional</h2>
+                <div>
+                    <h4>Raising A Village</h4>
+                    <p>
+                        <a href={"https://raiseavillage.org/"}>
+                            https://raiseavillage.org/
+                        </a>
+                    </p>
+                    <ul>
+                        <li>
+                            <p>Wordpress</p>
+                        </li>
+                    </ul>
+                </div>
+            </div>
         </div>
     );
 };
