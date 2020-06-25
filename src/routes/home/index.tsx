c886604a8682f606f9770d180f71820f65685e00
Trevor B <t.brixey0129@gmail.com>
2020-06-25 00:24:46 -0500

    personal site done I think

--- src/routes/home/index.tsx
+++ src/routes/home/index.tsx
@@ -5,22 +5,26 @@ import { Link } from "preact-router";
 const Home: FunctionalComponent = () => {
     return (
         <div class={style.container}>
-            <h1>My name is Trevor 🤙</h1>
+            <h1>My name is Trevor</h1>
             <h2>Full-Stack Developer and aspiring designer ✨</h2>
+            <h4 class={style.section}>
+                Check out my portfolio <Link href="/portfolio">here</Link>
+            </h4>
             <div class={style.section}>
                 I currently work at a small agency called{" "}
                 <a href={"https://crema.us"}>Crema</a> while also picking up
                 freelance jobs.
             </div>
-            <div class={style.section}>
-                Check out my portfolio{" "}
-                <Link
-                    href="/portfolio"
-                    style={{ color: "#147efb", textDecoration: "none" }}
-                >
-                    here
-                </Link>
+            <div class={style.longSection}>
+                I wanted to experiment with{" "}
+                <a href={"https://preactjs.com/"}>Preact</a> and Typescript so I
+                thought I might as well build my personal site using it.
+                I&apos;ve never had some fancy site because let&apos;s be
+                honest, you are here to check out my other work. Regardless,
+                this site is forever a WIP so come back and check it out from
+                time to time.
             </div>
+            <div class={style.section}>Thanks for stopping by 🤙</div>
         </div>
     );
 };
--- src/routes/portfolio/index.tsx
+++ src/routes/portfolio/index.tsx
@@ -5,9 +5,27 @@ const Portfolio: FunctionalComponent = () => {
     return (
         <div class={style.container}>
             <div class={style.section}>
-                <h2>Personal</h2>
+                <h2 style={{ marginBottom: "0px" }}>Personal</h2>
                 <div>
-                    <h4>Baby Companion</h4>
+                    <h4>All Affiliate Links (new)</h4>
+                    <p>Search for and submit affiliate codes!</p>
+                    <p>
+                        <a href={"https://allaffiliatelinks.com/"}>
+                            https://allaffiliatelinks.com/
+                        </a>
+                    </p>
+                    <ul>
+                        <li>
+                            <p>Preact</p>
+                        </li>
+                        <li>
+                            <p>Express</p>
+                        </li>
+                        <li>
+                            <p>Firebase / Heroku</p>
+                        </li>
+                    </ul>
+                    <h4>Baby Companion (In Development)</h4>
                     <ul>
                         <li>
                             <p>React Native</p>
@@ -19,9 +37,13 @@ const Portfolio: FunctionalComponent = () => {
                 </div>
             </div>
             <div class={style.section}>
-                <h2>Professional</h2>
+                <h2 style={{ marginBottom: "0px" }}>Professional</h2>
                 <div>
                     <h4>Raising A Village</h4>
+                    <p>
+                        Built for a non-profit organization that just needed
+                        something simple that people could go to.
+                    </p>
                     <p>
                         <a href={"https://raiseavillage.org/"}>
                             https://raiseavillage.org/
